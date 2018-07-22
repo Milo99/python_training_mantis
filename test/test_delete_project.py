becab31fb462f6950b16dfc63c253bb7685bace0
Kamil <kusiak.kamil@gmail.com>
2018-07-22 23:08:00 +0200

    Add / Delete Project

---
+++ test/test_delete_project.py
@@ -0,0 +1,15 @@
+from model.project import Project
+from random import randrange
+
+def test_delete_project(app):
+    if app.project.count() == 0:
+        app.project.create(Project(name="test"))
+
+    old_projects = app.project.get_projects_list()
+    index = randrange(len(old_projects))
+    project = old_projects[index]
+    app.project.delete_project_by_index(index)
+    new_projects = app.project.get_projects_list()
+    assert len(old_projects) - 1 == len(new_projects)
+    old_projects.remove(project)
+    assert sorted(old_projects) == sorted(new_projects)
